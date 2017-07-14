d48c603c18b54fbc8659cf0b5c8c572fa518fd97
BokuraNoMirai <hj_frese@yahoo.de>
2017-07-14 14:09:53 +0200

    save last save into flash

--- esp scripts/WindowContact/WindowContact.ino
+++ esp scripts/WindowContact/WindowContact.ino
@@ -3,17 +3,20 @@
 
 const char* ssid = "ZUCK";
 const char* password = "ruckzuck";
-const char* host = "NanoPiAir_Mailinh";
+const char* host = "zuck_server";
 const int httpPort = 8081;
+const int IDAddress = 0;
+const int bitAddress = 1;
+const int windowPos = 0;
+const int sendPos = 1;
 WiFiClient client;
 
 const int CONTACTPIN = 4;
-bool send = false;
 
 void setup() 
 {
   Serial.begin(115200);
-  EEPROM.begin(1);
+  EEPROM.begin(2);
   pinMode(CONTACTPIN, INPUT_PULLUP);
   
   delay(10);
@@ -24,11 +27,15 @@ void setup()
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
+  int timeout = millis() + 5000;
 
   while(WiFi.status() != WL_CONNECTED)
   {
-    delay(5000);
-    Serial.print(".");
+    if(timeout-millis() > 0)
+    {
+      delay(5000);
+      Serial.print(".");
+    }
   }
 
   Serial.println("");
@@ -39,68 +46,117 @@ void setup()
   Serial.println("Connecting to");
   Serial.println(host);
 
-  String windowOpen = "false";
-  boolean lastState = bitRead(EEPROM.read(1), 1);
-  
-  if(digitalRead(CONTACTPIN) == HIGH)
+  if (EEPROM.read(0) == 0)
   {
-    windowOpen = "true";
-    if(!lastState)
+    if (client.connect(host, httpPort))
     {
-      byte EEPROMBYTE = EEPROM.read(1);
-      bitWrite(EEPROMBYTE, 1, true);
-      EEPROM.write(1, EEPROMBYTE);
+      sendGetRequest("/sensor/signin/window");
+      String res = readServerResponse();
+      EEPROM.write(IDAddress, res.toInt());
       EEPROM.commit();
-      send = true;
-    }
-  }else
-  {
-    if(lastState)
-    {
-      byte EEPROMBYTE = EEPROM.read(1);
-      bitWrite(EEPROMBYTE, 1, false);
-      EEPROM.write(1, EEPROMBYTE);
-      EEPROM.commit();
-      send = true;
-    }
-  }
-  
-  if(send)
-  {
-    if(client.connect(host, httpPort))
-    {
-      String packet = "/sensor/window/" + windowOpen;
-      client.print(String("GET ") + packet + " HTTP/1.1\r\n" + "HOST: " + host + "\r\n" + "Connection: close\r\n\r\n");
-      Serial.println(packet);
-      send = false;
-
-      int timeout = millis() + 5000;
-        
-      while(client.available() == 0)
-      {
-        if(timeout - millis() < 0)
-        {
-          Serial.println("Timeout!");
-          client.stop();
-          return;
-        }
-      }
-      while(client.available())
-      {
-         String line = client.readStringUntil('\r');
-         Serial.println(line);
-      }
-    }else
+      writeBitToFlash(bitAddress, sendPos, true);
+    } else
     {
       Serial.println("Connection failed");
     }
-  }
+    ESP.deepSleep(5e6);
+  } else
+  {
+    String windowOpen = "false";
+    boolean sendLast = bitRead(EEPROM.read(bitAddress), sendPos);
+    boolean sendNow = false;
+    boolean lastState = bitRead(EEPROM.read(bitAddress), windowPos);
 
-  delay(1000);
-  ESP.deepSleep(240e6);
+    if(!sendLast)
+    {
+      if(lastState)
+        windowOpen = "true";
+      else
+        windowOpen = "false";
+
+      sendNow = true;
+    }else
+    {
+      if (digitalRead(CONTACTPIN) == HIGH)
+      {
+        windowOpen = "true";
+        if (!lastState)
+        {
+          writeBitToFlash(bitAddress, windowPos, true);
+          sendNow = true;
+        }
+      } else
+      {
+        if (lastState)
+        {
+          writeBitToFlash(bitAddress, windowPos, false);
+          sendNow = true;
+        }
+      }
+    }
+
+    if (sendNow)
+    {
+      if (client.connect(host, httpPort))
+      {
+        int id = EEPROM.read(IDAddress);
+        String packet = "/sensor/window/" + windowOpen;
+        packet = packet + "/";
+        packet = packet + id;
+        sendGetRequest(packet);
+        if(!sendLast)
+          writeBitToFlash(bitAddress, sendPos, true);
+
+        if(readServerResponse().toInt() == 0)
+        {
+          EEPROM.write(IDAddress, 0);
+          EEPROM.commit();
+        }
+      } else
+      {
+        Serial.println("Connection failed");
+        if(sendLast)
+          writeBitToFlash(bitAddress, sendPos, false);
+      }
+    }
+
+    delay(1000);
+    ESP.deepSleep(240e6);
+  }
 }
 
-void loop() 
+void writeBitToFlash(int address, int pos, bool value)
 {
-   
+  byte EEPROMBYTE = EEPROM.read(address);
+  bitWrite(EEPROMBYTE, pos, value);
+  EEPROM.write(address, EEPROMBYTE);
+  EEPROM.commit();
+}
+
+void sendGetRequest(String packet)
+{
+  client.print(String("GET ") + packet + " HTTP/1.1\r\n" + "HOST: " + host + "\r\n" + "Connection: close\r\n\r\n");
+}
+
+String readServerResponse()
+{
+  int timeout = millis() + 5000;
+
+  while (client.available() == 0)
+  {
+    if (timeout - millis() < 0)
+    {
+      Serial.println("Timeout!");
+      client.stop();
+      return "";
+    }
+  }
+  String preRes = client.readStringUntil('\z');
+  String res = client.readStringUntil('\z');
+  return res;
+}
+
+void loop()
+{
+
 }
